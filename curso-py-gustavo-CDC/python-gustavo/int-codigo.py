c6e4ad108390784282f4d76a31104c6be112431c
Camila Alves <camilabreu.contato@gmail.com>
2026-02-03 11:30:58 -0300

    Reorganiza pastas e remove estrutura antiga.

--- curso-py-gustavo-CDC/python-gustavo/int-codigo.py
+++
@@ -1,5 +0,0 @@
-# INTEIROS são números que não contenham nenhuma casa decimal é um tipo de dado inteiro em Python.
-
-variavel_int = 100
-print(variavel_int)
-print(type(variavel_int))  # Type mostra o tipo da variável
