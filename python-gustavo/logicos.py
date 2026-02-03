6a58c1253cbfd80069a351376064a22d955c62a1
Camila Alves <camilabreu.contato@gmail.com>
2026-02-03 10:14:33 -0300

    Organizando os estudos em subpastas

---
+++ python-gustavo/logicos.py
@@ -0,0 +1,8 @@
+# LOGICOS
+
+a = True
+b = True
+
+print(a and b)  # E lógico (todos os valores envolvidos devem ser True. Ex: a = True e b = True => a and b = True)
+print(a or b)   # OU lógico (pelo menos um dos valores envolvidos deve ser True. Ex: a = True ou b = False => a or b = True)
+print(not a)    # Negação lógica (inverte o valor lógico. Ex: a = True => not a = False)
